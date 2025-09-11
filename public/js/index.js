b0dbfc62f1f427b1da631b9fa2dbbcdc879bbe92
admin <sun88110@naver.com>
2025-09-11 16:17:50 +0900

    조건문 완료

--- public/js/index.js
+++ public/js/index.js
@@ -13,3 +13,21 @@ function changeWord() {
     //화면요소를 선택
     document.querySelector('h3#world').innerHTML = "안녕!";
 }
+
+//ondition.js에서 활용
+function changeValue(){
+
+let score = document.querySelector('#user_value').value;
+
+if(score >= 90){
+    console.log("아주 잘했습니다.")
+}else if (score >= 80){
+    console.log("잘했습니다.")
+}else if (score >= 70){
+    console.log("보통입니다.")
+}else if (score >= 60){
+    console.log("노력하세요.")
+}else{
+    console.log("낙제입니다 .")
+}
+}
